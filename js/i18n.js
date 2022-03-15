e25a955ded9b4e71f587aa7f4aaa44069014d8f3
asbarve <adityasbarve@gmail.com>
2022-03-15 15:49:44 -0400

    testing i18n

---
+++ js/i18n.js
@@ -0,0 +1,67 @@
+const lngs = {
+  en: { nativeName: 'English' },
+  th: { nativeName: 'Thai' }
+};
+
+const rerender = () => {
+  // start localizing, details:
+  // https://github.com/i18next/jquery-i18next#usage-of-selector-function
+  $('body').localize();
+}
+
+$(function () {
+  // use plugins and options as needed, for options, detail see
+  // https://www.i18next.com
+  i18next
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(i18nextBrowserLanguageDetector)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+      debug: true,
+      fallbackLng: 'en',
+      resources: {
+        en: {
+          translation: {
+            intro: {
+              title: 'Landing Page',
+              subTitle: 'Developing methods, technologies and prototypes to embed risk reduction and preparedness into the design of cities
+              to increase local community resilience.'
+            }
+          }
+        },
+        th: {
+          translation: {
+            intro: {
+              title: 'Webseite',
+              subTitle: 'Ein Untertitel'
+            }
+          }
+        }
+      }
+    }, (err, t) => {
+      if (err) return console.error(err);
+
+      // for options see
+      // https://github.com/i18next/jquery-i18next#initialize-the-plugin
+      jqueryI18next.init(i18next, $, { useOptionsAttr: true });
+
+      // fill language switcher
+      Object.keys(lngs).map((lng) => {
+        const opt = new Option(lngs[lng].nativeName, lng);
+        if (lng === i18next.resolvedLanguage) {
+          opt.setAttribute("selected", "selected");
+        }
+        $('#languageSwitcher').append(opt);
+      });
+      $('#languageSwitcher').change((a, b, c) => {
+        const chosenLng = $(this).find("option:selected").attr('value');
+        i18next.changeLanguage(chosenLng, () => {
+          rerender();
+        });
+      });
+
+      rerender();
+    });
+});
