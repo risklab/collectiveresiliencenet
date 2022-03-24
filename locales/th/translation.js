5deca207e945b97197f35c9df15297cda701e592
asbarve <asbarve@users.noreply.github.com>
2022-03-24 15:44:24 -0400

    Chore: all text converted to key-values

--- locales/th/translation.js
+++ locales/th/translation.js
@@ -1,11 +1,18 @@
 var locales_th = {
   translation: {
     language: 'Thai',
+    nav: {
+      home: 'Home',
+      about: 'About',
+      work: 'Work',
+      blog: 'News',
+      contact: 'Contact Us'
+    },
     intro: {
       header: '',
-      title: 'THBuilding Resilience Collectively',
-      subTitle: 'THDeveloping methods, technologies and prototypes to embed risk reduction and preparedness into the design of cities to increase local community resilience.',
-      aboutbtn: 'THMore about us',
+      title: 'Building Resilience Collectively',
+      subTitle: 'The Resilience Collective is an interdisciplinary hub of researchers and designers',
+      aboutbtn: 'More about us',
       getintouchbtn: 'Get in Touch'
     },
     about: {
@@ -14,23 +21,53 @@ var locales_th = {
       getintouchbtn: 'Get in Touch'
     },
     about_box: {
-      title: 'Strategies ✺ Strctures ✺ Systems',
+      title: 'Strategies - Strctures - Systems',
       subTitle: 'With a global network of partners, the Lab is a place to innovate on techniques, processes, and systems to address the complexities of social, climatic, and hydrologic risks. We engage in action research to focus on the needs of diverse cultures and contexts.'
     },
     team: {
-      header: 'OUR TEAM',
+      header: 'TEAM',
       title: 'The Resilience Collective is an interdisciplinary network of researchers and designers',
-      subTitle: 'We aspire to change the course of current global development trends through a radical shift in research and action to proactively embed preparedness, risk reduction, and resilience capacity in this rapidly urbanizing world.',
+      p_text: 'We aspire to change the course of current global development trends through a radical shift in research and action to proactively embed preparedness, risk reduction, and resilience capacity in this rapidly urbanizing world.',
       aboutbtn: 'More about our team'
     },
     portfolio: {
-      header: 'WORKS',
+      header: 'Projects',
       title: 'Pilot Projects',
-      aboutbtn: 'Show More'
+      aboutbtn: 'Show More',
+      thRiskmap: 'RiskMap Thailand',
+      vaccineMap: 'VaccineMap',
+      toolkit: 'Collective Mapping toolkit',
     },
     blog: {
-      header: 'Blog & Media',
-      title: 'Our News'
+      header: 'Stay updated',
+      title: 'News',
+      news_header1:'Launched RiskMap Thailand',
+      news_date1:'Some date',
+      news_text1:'some text',
+      news_header2:'Deployed VaccineMap',
+      news_date2:'Some date',
+      news_text2:'some text',
+      news_header3:'Presented at Conference',
+      news_date3:'Some date',
+      news_text3:'some text',
+    },
+    contact: {
+      header: 'CONTACT US',
+      title:'Get in Touch',
+      email:'Your email',
+      subject: 'Subject',
+      textarea: 'Message here...',
+      sendbtn: 'Send Message'
+    },
+    footer: {
+      title:'Affiliated With',
+      header:'MIT URBAN RISK LAB',
+      home:'Home',
+      about:'About',
+      work:'Work',
+      news:'News',
+      contact:'Contact',
+      attribution:'© 2022 All Right Reserved'
     }
   }
 }
